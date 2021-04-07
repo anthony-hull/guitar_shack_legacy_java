479cc2effff138c79fef3f9c502bcbbc8407d333
Anthony Hull <anthony.hull@ford.com>
2021-04-07 15:45:55 +0100

    created unit tests for StockMonitor

---
+++ src/test/java/com/guitarshack/StockMonitorTest.java
@@ -0,0 +1,37 @@
+package com.guitarshack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+public class StockMonitorTest {
+
+
+    private Alert alert;
+    private StockMonitor stockMonitor;
+
+    @Before
+    public void setup() {
+        alert = mock(Alert.class);
+        Service service = mock(Service.class);
+        String productJSON = "{\"id\":811,\"stock\":29,\"leadTime\":14}";
+        String salesJSON = "{\"total\":60}";
+        when(service.fetchJSON(any(), any())).thenReturn(productJSON, salesJSON);
+        stockMonitor = new StockMonitor(alert, service);
+    }
+
+    @Test
+    public void alertIsSent() {
+        stockMonitor.productSold(811, 1);
+        verify(alert).send(any());
+    }
+
+
+    @Test
+    public void alertIsNotSent() {
+        stockMonitor.productSold(811,0);
+        verify(alert,never()).send(any());
+    }
+}
